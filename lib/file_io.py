dc11c4f11b8b948ea4e1d9365811901ff7a29f67
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-11-23 17:33:00 +0300

    Complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, mode='w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, mode='a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'r') as f:
+        content = f.read()
+    return content
